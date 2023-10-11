2d069d309420fa208cbf723828f179a981f37a54
RunThem <iccy.fun@outlook.com>
2023-10-12 05:47:49 +0800

    add libsock.c.h source file

---
+++ src/libsock.c
@@ -0,0 +1,268 @@
+#include "libsock.h"
+
+/* libs */
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#undef u_if
+#define u_if(expr, _ret)                                                                           \
+  do {                                                                                             \
+    if (expr) {                                                                                    \
+      ret = _ret;                                                                                  \
+      goto err;                                                                                    \
+    }                                                                                              \
+  } while (0)
+
+typedef struct sock_info_t sock_info_t;
+struct sock_info_t {
+  int family;
+  int type;
+  int protocol;
+
+  char* host;
+  size_t host_size;
+
+  in_port_t port;
+};
+
+static int sock_check(sock_conf_t* conf, sock_info_t* info) {
+  ret_t ret = SOCK_RET_OK;
+
+  /* check port */
+  u_if(conf->port > UINT16_MAX, SOCK_RET_CHECK);
+  info->port = conf->port;
+
+  /* TODO: check host */
+  info->host      = conf->host;
+  info->host_size = strlen(conf->host);
+
+  u_if(conf->type < SOCK_TYPE_INET4_TCP || conf->type > SOCK_TYPE_LOCAL_UDP, SOCK_RET_CHECK);
+
+#define set(_family, _type, _protocol)                                                             \
+  do {                                                                                             \
+    info->family   = _family;                                                                      \
+    info->type     = _type;                                                                        \
+    info->protocol = _protocol;                                                                    \
+  } while (0)
+
+  if (conf->type == SOCK_TYPE_INET4_TCP) {
+    set(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  } else if (conf->type == SOCK_TYPE_INET4_UDP) {
+    set(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  } else if (conf->type == SOCK_TYPE_INET6_TCP) {
+    set(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+  } else if (conf->type == SOCK_TYPE_INET6_UDP) {
+    set(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+  } else if (conf->type == SOCK_TYPE_LOCAL_TCP) {
+    set(AF_LOCAL, SOCK_STREAM, 0);
+  } else if (conf->type == SOCK_TYPE_LOCAL_UDP) {
+    set(AF_LOCAL, SOCK_DGRAM, 0);
+  }
+#undef set
+
+  return SOCK_RET_OK;
+
+err:
+  return ret;
+}
+
+static int sock_set_opt(sock_conf_t* conf, int fd) {
+  ret_t ret       = 0;
+  sock_opt_t* opt = NULL;
+
+  for (size_t i = 0; i < SOCK_MAX_OPTS; i++) {
+    opt = &conf->opts[i];
+    if (opt->opt_id == 0) {
+      break;
+    }
+
+    ret = setsockopt(fd, SOL_SOCKET, opt->opt_id, opt->value, opt->value_len);
+    u_if(ret == -1, SOCK_RET_SETSOCKOPT);
+  }
+
+  return SOCK_RET_OK;
+
+err:
+  return ret;
+}
+
+static int sock_set_nonblock(sock_conf_t* conf) {
+  ret_t ret = 0;
+  int flags = 0;
+
+  ret = fcntl(conf->fd, F_GETFL, 0);
+  u_if(ret == -1, SOCK_RET_FCNTL);
+
+  ret = fcntl(conf->fd, F_SETFL, flags | O_NONBLOCK);
+  u_if(ret == -1, SOCK_RET_FCNTL);
+
+  return SOCK_RET_OK;
+
+err:
+  return ret;
+}
+
+static int sock_open_inet(sock_conf_t* conf, sock_info_t* info) {
+  ret_t ret             = 0;
+  int fd                = -1;
+  char port_buf[6]      = {0};
+  struct addrinfo hints = {0};
+  struct addrinfo* res  = NULL;
+  struct addrinfo* ai   = NULL;
+
+  hints.ai_family   = info->family;
+  hints.ai_socktype = info->type;
+  hints.ai_protocol = info->protocol;
+
+  /* set passive unless UDP client */
+  if (!(info->type == SOCK_DGRAM && conf->listen == 0)) {
+    hints.ai_flags |= AI_PASSIVE;
+  }
+
+  if (info->type != AF_LOCAL) {
+    hints.ai_flags |= AI_NUMERICHOST;
+  }
+
+  snprintf(port_buf, sizeof(port_buf), "%u", conf->port);
+
+  ret = getaddrinfo(conf->host, port_buf, &hints, &res);
+  u_if(ret != 0, SOCK_RET_GETADDRINFO);
+
+  for (ai = res; ai != NULL; ai = ai->ai_next) {
+    fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    if (fd == -1) {
+      ret = SOCK_RET_SOCKET;
+      continue;
+    }
+
+    ret = sock_set_opt(conf, fd);
+    u_if(ret != SOCK_RET_OK, ret);
+
+    /* server */
+    if (conf->listen > 0) {
+      ret = bind(fd, res->ai_addr, res->ai_addrlen);
+      u_if(ret == -1, SOCK_RET_BIND);
+
+      /* TCP server */
+      if (info->type == SOCK_STREAM) {
+        ret = listen(fd, conf->listen);
+        u_if(ret == -1, SOCK_RET_LISTEN);
+      }
+    } else { /* client */
+      if (info->type == SOCK_DGRAM) {
+        break;
+      }
+
+      ret = connect(fd, ai->ai_addr, ai->ai_addrlen);
+      if (ret == 0) {
+        break;
+      }
+
+      close(fd);
+      fd  = -1;
+      ret = SOCK_RET_CONNECT;
+    }
+  }
+
+  u_if(fd == -1, ret);
+
+  freeaddrinfo(res);
+  conf->fd = fd;
+
+  return 0;
+
+err:
+  if (fd != -1) {
+    close(fd);
+  }
+
+  freeaddrinfo(res);
+
+  return ret;
+}
+
+static int sock_open_unix(sock_conf_t* conf, sock_info_t* info) {
+  ret_t ret               = 0;
+  int fd                  = 0;
+  struct sockaddr_un addr = {0};
+  socklen_t addr_len      = sizeof(addr);
+
+  fd = socket(info->family, info->type, info->protocol);
+  u_if(fd == -1, SOCK_RET_SOCKET);
+
+  ret = sock_set_opt(conf, fd);
+  u_if(ret != SOCK_RET_OK, ret);
+
+  addr.sun_family = AF_LOCAL;
+  strncpy(addr.sun_path, info->host, info->host_size);
+
+  /* server */
+  if (conf->listen > 0) {
+    ret = bind(fd, (struct sockaddr*)&addr, addr_len);
+    u_if(ret == -1, SOCK_RET_BIND);
+
+    if (info->type == SOCK_STREAM) {
+      ret = listen(fd, conf->listen);
+      u_if(ret == -1, SOCK_RET_LISTEN);
+    }
+  } else {
+    ret = connect(fd, (struct sockaddr*)&addr, addr_len);
+    u_if(ret == -1, SOCK_RET_CONNECT);
+  }
+
+  conf->fd = fd;
+
+  return 0;
+
+err:
+  if (fd != -1) {
+    close(fd);
+  }
+
+  return ret;
+}
+
+int sock_open(sock_conf_t* conf) {
+  ret_t ret        = 0;
+  sock_info_t info = {0};
+
+  u_if(conf == NULL, SOCK_RET_NULL);
+
+  ret = sock_check(conf, &info);
+  u_if(ret != SOCK_RET_OK, ret);
+
+  conf->fd = -1;
+  switch (conf->type) {
+    case SOCK_TYPE_LOCAL_TCP:
+    case SOCK_TYPE_LOCAL_UDP:
+      ret = sock_open_unix(conf, &info);
+      break;
+    case SOCK_TYPE_INET4_TCP:
+    case SOCK_TYPE_INET4_UDP:
+    case SOCK_TYPE_INET6_TCP:
+    case SOCK_TYPE_INET6_UDP:
+      ret = sock_open_inet(conf, &info);
+      break;
+
+    default:
+      break;
+  }
+
+  u_if(ret != SOCK_RET_OK, ret);
+
+  if (ret == SOCK_RET_OK && conf->nonblock) {
+    ret = sock_set_nonblock(conf);
+    u_if(ret != SOCK_RET_OK, ret);
+  }
+
+  return SOCK_RET_OK;
+
+err:
+  return ret;
+}
---
+++ src/libsock.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define SOCK_MAX_OPTS 4
+
+typedef int ret_t;
+
+typedef struct sock_opt_t sock_opt_t;
+struct sock_opt_t {
+  int opt_id;
+  void* value;
+  socklen_t value_len;
+};
+
+typedef struct sock_conf_t sock_conf_t;
+struct sock_conf_t {
+  int fd;
+
+#define SOCK_TYPE_INET4_TCP 1
+#define SOCK_TYPE_INET6_TCP 2
+#define SOCK_TYPE_INET4_UDP 3
+#define SOCK_TYPE_INET6_UDP 4
+#define SOCK_TYPE_LOCAL_TCP 5
+#define SOCK_TYPE_LOCAL_UDP 6
+  int type;
+
+  char* host;
+  in_port_t port;
+  int listen;
+  bool nonblock;
+
+  sock_opt_t opts[SOCK_MAX_OPTS];
+};
+
+#define SOCK_RET_OK          0 /* Ok */
+#define SOCK_RET_NULL        1 /* Pointer is NULL */
+#define SOCK_RET_CHECK       2 /* Paramerter error */
+#define SOCK_RET_SOCKET      5 /* socket() call failed */
+#define SOCK_RET_BIND        6 /* bind() call failed */
+#define SOCK_RET_LISTEN      7 /* listen() call failed */
+#define SOCK_RET_CONNECT     8 /* connect() call failed */
+#define SOCK_RET_FCNTL       3 /* fcntl() call failed */
+#define SOCK_RET_SETSOCKOPT  4 /* setsockopt() call failed */
+#define SOCK_RET_GETADDRINFO 5 /* getaddrinfo() call failed */
+
+int sock_open(sock_conf_t* conf);
