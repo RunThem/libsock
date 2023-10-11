9096238f5ab00c6a949e27997db7b0103488059e
RunThem <iccy.fun@outlook.com>
2023-10-12 05:47:08 +0800

    update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -8,7 +8,7 @@ set_version('0.0.1')
 set_xmakever('2.6.1')
 
 --- Build mode
-add_rules('mode.debug', 'mode.valgrind', 'mode.release')
+add_rules('mode.debug', 'mode.release')
 
 --- Macro definition
 add_defines('_GNU_SOURCE=1')
@@ -17,54 +17,22 @@ add_defines('_GNU_SOURCE=1')
 set_warnings('all', 'error')
 
 --- Language standard
-set_languages('clatest', 'cxxlatest')
-
---- Unused variables and functions
-add_cflags('-Wno-unused-function', '-Wno-unused-variable', '-Wno-unused-but-set-variable')
-
---- DWARF v4
-add_cflags('-gdwarf-4')
-
---- Toolchain
-add_toolchains('clang')
-
---- Task(lsp) generate the project file
-task('lsp', function()
-  set_category('plugin')
-
-  on_run(function()
-    os.exec('xmake project -k cmake build/lsp')
-    os.exec('xmake project -k compile_commands build/lsp')
-  end)
-
-  set_menu({
-    usage = 'xmake lsp',
-    description = 'Generate the project file.',
-  })
-end)
-
---- Lambda expressions
-local lambda = false
-if lambda then
-  add_cflags('-fblocks')
-  if is_plat('linux') then
-    add_ldflags('-lBlocksRuntime')
-    add_defines('__linux__=1')
-  end
-end
-
---- Private repositories
-add_repositories('RunThem https://github.com/RunThem/My-xmake-repo')
-add_requires('libu')
-add_requires('mimalloc')
+set_languages('c99')
 
 --- Project common header file path
 add_includedirs('$(projectdir)/src')
 
 --- main target
 target('libsock', function()
-  set_kind('binary')
-  add_files('src/*.c')
+  set_kind('static')
+  add_files('src/libsock.c')
+  add_headerfiles('src/libsock.h')
+end)
 
-  add_packages('libu', 'mimalloc')
+target('test', function()
+  set_kind('binary')
+  set_default(false)
+  add_files('src/test.c')
+
+  add_deps('libsock')
 end)
